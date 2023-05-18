e690f59b8e8bc5b6b55ab132db6a6e0227bf1289
Quinn Booth <qabooth@gmail.com>
2023-05-18 01:36:09 -0400

    Loading basic images
    Load images and process them into a cell class.

--- collapse.js
+++ collapse.js
@@ -3,6 +3,22 @@
 //  Author: Quinn Booth
 //
 
+var cells = [];
+var images = [];
+let image_set = "basic";
+var generate_ready = true;
+
+const image_count = {
+    basic: 6
+}
+
+class Cell {
+    constructor(image, colors) {
+        this.image = image;
+        this.colors = colors;
+    }
+}
+
 //#region HTML Setup
 
 // Create canvas for wavefunction collapse
@@ -11,12 +27,12 @@ const collapse_container = document.getElementById('collapse_container');
 var canvas = document.createElement("canvas");
 collapse_container.appendChild(canvas);
 
-var maxHeight = Math.floor(collapse_container.clientHeight / 100);
-var maxWidth = Math.floor(collapse_container.clientWidth / 100);
+var maxHeight = Math.floor(collapse_container.clientHeight / 50);
+var maxWidth = Math.floor(collapse_container.clientWidth / 50);
 collapse_width = Math.floor(maxWidth * 0.75);
 collapse_height = Math.floor(maxHeight * 0.75);
-canvas.width = String(collapse_width * 100);
-canvas.height = String(collapse_height * 100);
+canvas.width = String(collapse_width * 50);
+canvas.height = String(collapse_height * 50);
 
 var ctx = canvas.getContext("2d");
 draw_gridlines();
@@ -35,7 +51,7 @@ window_height.addEventListener('input', (event) => {
             event.target.value = maxHeight;
         }
         collapse_height = event.target.value;
-        canvas.height = String(collapse_height * 100);
+        canvas.height = String(collapse_height * 50);
     }
     draw_gridlines();
 });
@@ -49,7 +65,7 @@ window_width.addEventListener('input', (event) => {
             event.target.value = maxWidth;
         }
         collapse_width = event.target.value;
-        canvas.width = String(collapse_width * 100);
+        canvas.width = String(collapse_width * 50);
     }
     draw_gridlines();
 });
@@ -75,17 +91,17 @@ window_width.addEventListener('focus', (event) => {
 var abort_generation = false;
 
 window.addEventListener('resize', function() {
-    maxHeight = Math.floor(collapse_container.clientHeight / 100);
-    maxWidth = Math.floor(collapse_container.clientWidth / 100);
+    maxHeight = Math.floor(collapse_container.clientHeight / 50);
+    maxWidth = Math.floor(collapse_container.clientWidth / 50);
     if (collapse_height > maxHeight) {
         collapse_height = maxHeight;
         window_height.value = collapse_height;
-        canvas.height = String(collapse_height * 100);
+        canvas.height = String(collapse_height * 50);
     }
     if (collapse_width > maxWidth) {
         collapse_width = maxWidth;
         window_width.value = collapse_width;
-        canvas.width = String(collapse_width * 100);
+        canvas.width = String(collapse_width * 50);
     }
     draw_gridlines()
 });
@@ -98,20 +114,113 @@ function draw_gridlines() {
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "black";
     
-    const cellDim = 100;
+    const cellDim = 50;
     const fillDim = cellDim - 2;
-    x_cells = collapse_width;
-    y_cells = collapse_height;
+    let x_cells = collapse_width;
+    let y_cells = collapse_height;
 
     for (let i = 0; i < x_cells; i++) {
         for (let j = 0; j < y_cells; j++) {
-            ctx.fillRect(i * 100 + 1, j * 100 + 1, fillDim, fillDim);
+            ctx.fillRect(i * cellDim + 1, j * cellDim + 1, fillDim, fillDim);
         }
     }
 
     ctx.fillStyle = "#E7DFDD";
 }
 
-function generate() {
-    
+function load_images(image_set) {
+
+    generate_ready = false;
+    draw_gridlines();
+    const folder = `images/${image_set}`;
+    var loaded = 0;
+  
+    function imageLoaded() {
+      loaded++;
+      if (loaded === image_count[image_set]) {
+        generate_ready = true;
+      }
+    }
+  
+    for (let i = 0; i < image_count[image_set]; i++) {
+      var image = new Image();
+      image.crossOrigin = "Anonymous";
+      image.onload = imageLoaded;
+      image.src = `${folder}/${image_set}${i}.png`;
+      images.push(image);
+    }
 }
+
+function get_rgb(image, callback) {
+
+    // Setup a blank testing canvas to read image data
+    let test_canvas = document.createElement("canvas");
+    test_canvas.width = 50;
+    test_canvas.height = 50;
+    let test_ctx = test_canvas.getContext("2d", { willReadFrequently: true });
+    test_ctx.drawImage(image, 0, 0, 50, 50);
+
+    // Make sure image loads before testing for RGB values
+    setTimeout(() => {
+
+        const rgb_top1 = test_ctx.getImageData(12, 0, 1, 1).data;
+        const rgb_top2 = test_ctx.getImageData(25, 0, 1, 1).data;
+        const rgb_top3 = test_ctx.getImageData(36, 0, 1, 1).data;
+
+        const rgb_right1 = test_ctx.getImageData(49, 12, 1, 1).data;
+        const rgb_right2 = test_ctx.getImageData(49, 25, 1, 1).data;
+        const rgb_right3 = test_ctx.getImageData(49, 36, 1, 1).data;
+
+        const rgb_bottom1 = test_ctx.getImageData(36, 49, 1, 1).data;
+        const rgb_bottom2 = test_ctx.getImageData(25, 49, 1, 1).data;
+        const rgb_bottom3 = test_ctx.getImageData(12, 49, 1, 1).data;
+
+        const rgb_left1 = test_ctx.getImageData(0, 36, 1, 1).data;
+        const rgb_left2 = test_ctx.getImageData(0, 25, 1, 1).data;
+        const rgb_left3 = test_ctx.getImageData(0, 12, 1, 1).data;
+      
+        let rgb_top = rgb_top1.slice(0, 3).join('') + rgb_top2.slice(0, 3).join('') + rgb_top3.slice(0, 3).join('');
+        let rgb_right = rgb_right1.slice(0, 3).join('') + rgb_right2.slice(0, 3).join('') + rgb_right3.slice(0, 3).join('');
+        let rgb_bottom = rgb_bottom1.slice(0, 3).join('') + rgb_bottom2.slice(0, 3).join('') + rgb_bottom3.slice(0, 3).join('');
+        let rgb_left = rgb_left1.slice(0, 3).join('') + rgb_left2.slice(0, 3).join('') + rgb_left3.slice(0, 3).join('');
+
+        callback(image, [rgb_top, rgb_right, rgb_bottom, rgb_left]);
+
+    }, 10);
+
+}
+
+function load_cells(callback) {
+
+    if (!generate_ready) {
+        window.setTimeout(load_cells, 100, callback);
+        return;
+    }
+
+    for (let i = 0; i < images.length; i++) {
+
+        get_rgb(images[i], function (image, rgb) {
+            let cell = new Cell(image, rgb);
+            cells.push(cell);
+        });
+        
+    }
+        
+    callback();
+
+}
+
+
+function generate() {
+
+    if (!generate_ready) return;
+
+    ctx.drawImage(images[4], 0, 0, 50, 50);
+
+}
+
+load_images(image_set);
+load_cells(function () {
+    console.log(cells);
+});
+
