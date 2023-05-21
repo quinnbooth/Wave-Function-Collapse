542b94e53164521c4d720389978967ef356d7991
Quinn Booth <qabooth@gmail.com>
2023-05-21 00:54:54 -0400

    Instructions and cleanup
    Added link to GitHub repo on website for detailed upload instructions. Commented and cleaned up the code base.

--- collapse.js
+++ collapse.js
@@ -7,19 +7,24 @@ var cells = [];
 var images = [];
 var rotated_images = [];
 var matrix = [];
-var image_set = "farm";
+var image_set = "basic";
+var generation_mode = "slow";
 var generate_ready = true;
 var cellSize = 50;
 
+// Number of images in each preset
 const image_count = {
+
     basic: 6,
     circuit: 12,
     roots: 12,
     octo: 8,
     blob: 7,
     farm: 11
+
 }
 
+// Stores basic information about an image and which other images it can be neighbors with.
 class Cell {
 
     constructor(image, colors) {
@@ -35,14 +40,15 @@ class Cell {
 
 }
 
+// One per square in the grid printed on the website. Stores which cells can possibly go in its location.
 class Box {
 
     constructor(total_cells, x, y) {
 
         this.done = false;
+        this.possibilities = [];
         this.x = x;
         this.y = y;
-        this.possibilities = [];
 
         for (let i = 0; i < total_cells; i++) {
 
@@ -55,11 +61,11 @@ class Box {
 //#region HTML Setup
 
 // Create canvas for wavefunction collapse
-
 const collapse_container = document.getElementById('collapse_container');
 var canvas = document.createElement("canvas");
 collapse_container.appendChild(canvas);
 
+// Set dimensions of canvas such that they are less than a maximum threshold and can fit an integer number of 50 x 50 squares.
 var heightCap = 10;
 var widthCap = 12;
 var maxHeight = Math.min(Math.floor(collapse_container.clientHeight / 50), heightCap);
@@ -69,16 +75,14 @@ collapse_height = Math.min(maxHeight, heightCap);
 canvas.width = String(collapse_width * 50);
 canvas.height = String(collapse_height * 50);
 
-var generate_button = document.getElementById("generate_button");
-
 var ctx = canvas.getContext("2d");
 draw_gridlines();
 
-// Maintain proper heights and weights
-
+// Provide user control over dimensions within the above constraints.
 const window_height = document.getElementById('height_field');
 const window_width = document.getElementById('width_field');
-          
+
+// User changes height
 window_height.addEventListener('input', (event) => {
     height = event.target.value
     if (height !== '') {
@@ -93,6 +97,7 @@ window_height.addEventListener('input', (event) => {
     draw_gridlines();
 });
 
+// User changes width
 window_width.addEventListener('input', (event) => {
     width = event.target.value
     if (width !== '') {
@@ -107,6 +112,7 @@ window_width.addEventListener('input', (event) => {
     draw_gridlines();
 });
 
+// User clicks on height field for the first time, display height
 window_height.addEventListener('focus', (event) => {
     window_height.placeholder = '';
     if (event.target.value === '') {
@@ -115,6 +121,7 @@ window_height.addEventListener('focus', (event) => {
     draw_gridlines();
 });
 
+// User clicks on width field for the first time, display width
 window_width.addEventListener('focus', (event) => {
     window_width.placeholder = '';
     if (event.target.value === '') {
@@ -123,8 +130,7 @@ window_width.addEventListener('focus', (event) => {
     draw_gridlines();
 });
 
-// Resize canvas and image selector when window is resized
-
+// Recalculate dimension constraints and resize canvas when window is resized.
 window.addEventListener('resize', function() {
     maxHeight = Math.min(Math.floor(collapse_container.clientHeight / 50), heightCap);
     maxWidth = Math.min(Math.floor(collapse_container.clientWidth / 50), widthCap);
@@ -141,6 +147,7 @@ window.addEventListener('resize', function() {
     draw_gridlines()
 });
 
+// Resize the image selection panel when the window is resized.
 const left_panel = document.getElementById('left_panel');
 const image_selector = document.getElementById('image_selector');
 
@@ -155,8 +162,7 @@ window.addEventListener('resize', function() {
 });
 
 // Swap between slow and fast generation modes
-
-var generation_mode = "slow";
+const generate_button = document.getElementById("generate_button");
 const instant_button = document.getElementById('instant_button');
 instant_button.addEventListener('click', function() {
     instant_button.classList.toggle('button-active');
@@ -183,7 +189,6 @@ instant_button.addEventListener('click', function() {
 });
 
 // Make image set previews clickable so user can select one
-
 const basic_selector = document.getElementById('basic_selector');
 const circuit_selector = document.getElementById('circuit_selector');
 const roots_selector = document.getElementById('roots_selector');
@@ -209,11 +214,11 @@ for (let i = 0; i < selectors.length; i++) {
 }
 
 // Make clickable button that prompts for custom image set upload
-
 function upload() {
     document.getElementById('upload').click();
 }
 
+// Add upload functionality
 const upload_field = document.getElementById('upload');
 
 upload_field.addEventListener('change', (event) => {
@@ -225,6 +230,7 @@ upload_field.addEventListener('change', (event) => {
     images = [];
     var loaded = 0;
 
+    // Update this count when the images load asynchronously, make a flag true when all are done.
     function imageLoaded() {
         loaded++;
         console.log(loaded);
@@ -233,6 +239,7 @@ upload_field.addEventListener('change', (event) => {
         }
     }
 
+    // Open a file reader for each image in the upload, store it in an Image() inside of the images list.
     for (let i = 0; i < uploaded_images.length; i++) {
 
         const uploaded_image = uploaded_images[i];
@@ -252,16 +259,18 @@ upload_field.addEventListener('change', (event) => {
         reader.readAsDataURL(uploaded_image);
     }
 
+    // Get rotations of the images and load them into cells
     get_rotations(images);
     setTimeout(() => {
         load_cells(function () {
             setTimeout(() => {
+
+                // Find which cells are allowed to be neighbors.
                 determine_constraints();
                 generate_ready = true;
                 generate_button.textContent = "GENERATE";
 
-                console.log(images);
-
+                // Draw the images the user uploaded on the canvas for confirmation.
                 let x = 0;
                 let y = 0;
                 for (let i = 0; i < images.length; i++) {
@@ -277,12 +286,18 @@ upload_field.addEventListener('change', (event) => {
     }, 1000);
 });
 
+// Allow user to click entire button around link to visit repo.
+function goto_repo() {
+    document.getElementById('repo_link').click();
+}
+
 //#endregion
 
 //#region Loading Images
 
 function draw_gridlines() {
 
+    // Clear the canvas.
     ctx.fillStyle = "#442b9e";
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     ctx.fillStyle = "black";
@@ -292,6 +307,7 @@ function draw_gridlines() {
     let x_cells = collapse_width;
     let y_cells = collapse_height;
 
+    // Draw horizontal and vertical lines at even intervals.
     for (let i = 0; i < x_cells; i++) {
         for (let j = 0; j < y_cells; j++) {
             ctx.fillRect(i * cellDim + 1, j * cellDim + 1, fillDim, fillDim);
@@ -312,6 +328,7 @@ function load_images(image_set) {
     const folder = `images/${image_set}`;
     var loaded = 0;
   
+    // Update this count when the images load asynchronously, make a flag true when all are done.
     function imageLoaded() {
       loaded++;
       if (loaded === image_count[image_set]) {
@@ -319,12 +336,15 @@ function load_images(image_set) {
       }
     }
   
+    // For each image in the selected set, store it in an Image() in the images list.
     for (let i = 0; i < image_count[image_set]; i++) {
+
       var image = new Image();
       image.crossOrigin = "Anonymous";
       image.onload = imageLoaded;
       image.src = `${folder}/${image_set}${i}.png`;
       images.push(image);
+
     }
 
 }
@@ -341,6 +361,7 @@ function get_rgb(image, callback) {
     // Make sure image loads before testing for RGB values
     setTimeout(() => {
 
+        // Test three points on every side of the image for their rgb values.
         const rgb_top1 = test_ctx.getImageData(12, 0, 1, 1).data;
         const rgb_top2 = test_ctx.getImageData(25, 0, 1, 1).data;
         const rgb_top3 = test_ctx.getImageData(37, 0, 1, 1).data;
@@ -357,6 +378,7 @@ function get_rgb(image, callback) {
         const rgb_left2 = test_ctx.getImageData(0, 25, 1, 1).data;
         const rgb_left3 = test_ctx.getImageData(0, 37, 1, 1).data;
       
+        // Join the rgb value readings for a color scheme identifier for that edge.
         let rgb_top = rgb_top1.slice(0, 3).join('') + rgb_top2.slice(0, 3).join('') + rgb_top3.slice(0, 3).join('');
         let rgb_right = rgb_right1.slice(0, 3).join('') + rgb_right2.slice(0, 3).join('') + rgb_right3.slice(0, 3).join('');
         let rgb_bottom = rgb_bottom1.slice(0, 3).join('') + rgb_bottom2.slice(0, 3).join('') + rgb_bottom3.slice(0, 3).join('');
@@ -368,11 +390,11 @@ function get_rgb(image, callback) {
 
 }
 
-function get_rotations(images, callback) {
+function get_rotations(images) {
 
     // Wait until images are loaded
     if (!generate_ready) {
-        window.setTimeout(get_rotations, 100, images/*, callback*/);
+        window.setTimeout(get_rotations, 100, images);
         return;
     }
 
@@ -382,15 +404,8 @@ function get_rotations(images, callback) {
     generate_ready = false;
     rotated_images = [];
 
-    function addRotation(count, rot) {
+    function addRotation(rot) {
         rotated_images.push(rot);
-
-        // Load cells once all images have been rotated
-        /*if (count >= images.length - 1) {
-            setTimeout(() => {
-                callback();
-            }, 10);
-        }*/
     }
 
     for (let i = 0; i < images.length; i++) {
@@ -427,6 +442,7 @@ function get_rotations(images, callback) {
         // Make sure images load before testing differences
         setTimeout(() => {
 
+            // Grab all the rgb data for each rotation
             let rgb0 = ctx0.getImageData(0, 0, canvas0.width, canvas0.height).data;
             let rgb90 = ctx90.getImageData(0, 0, canvas90.width, canvas90.height).data;
             let rgb180 = ctx180.getImageData(0, 0, canvas180.width, canvas180.height).data;
@@ -485,7 +501,7 @@ function get_rotations(images, callback) {
                 if (unique_rotations[j] == j + 1) {
                     const rotated_image = new Image();
                     rotated_image.crossOrigin = "Anonymous";
-                    rotated_image.onload = addRotation(i, rotated_image);
+                    rotated_image.onload = addRotation(rotated_image);
                     rotated_image.src = rgb_list[j].toDataURL();
                 }
             }
@@ -497,12 +513,17 @@ function get_rotations(images, callback) {
 function load_cells(callback) {
 
     // console.log("Loading images into cells.")
+
     cells = [];
 
+    // Get the rgb identifiers for each rotated images and put it in a Cell() along with the image.
     for (let i = 0; i < rotated_images.length; i++) {
+
         get_rgb(rotated_images[i], function (image, rgb) {
+
             let cell = new Cell(image, rgb);
             cells.push(cell);
+
         });   
     }
 
@@ -547,25 +568,19 @@ function determine_constraints() {
 }
 
 function load_new_images() {
+
+    // Get rotations of the images and load them into cells
     load_images(image_set);
-
-    // get_rotations(images, function () {
-    //     load_cells(function () {
-    //         setTimeout(() => {
-    //             determine_constraints();
-    //             generate_ready = true;
-    //             generate_button.textContent = "GENERATE";
-    //         }, 10);
-    //     });
-    // });
-
     get_rotations(images);
     setTimeout(() => {
         load_cells(function () {
             setTimeout(() => {
+
+                // Find which cells are allowed to be neighbors.
                 determine_constraints();
                 generate_ready = true;
                 generate_button.textContent = "GENERATE";
+
             }, 100);
         });
     }, 1000);
@@ -577,31 +592,40 @@ function load_new_images() {
 
 function draw_current_state(matrix, mode) {
 
+    // Only show final product of wave function collapse.
     if (mode == "fast") {
 
+        // Clear canvas and draw gridlines.
         draw_gridlines();
 
+            // For each box in the grid, check to see if it's done (been determined) and draw its corresponding image if so.
             for (let i = 0; i < matrix.length; i++) {
                 for (let j = 0; j < matrix[i].length; j++) {
+
                     let current_box = matrix[i][j];
                     if (current_box.done == true) {
                         ctx.drawImage(rotated_images[current_box.possibilities[0]], j * 50, i * 50, cellSize, cellSize);
                     }
+
                 }
             }
 
+    // Draw each intermittent step of the algorithm so user can see it building.
     } else {
 
+        // Same procedure as above, but now in a promise to make program wait between stages.
         return new Promise(resolve => {
 
             draw_gridlines();
 
             for (let i = 0; i < matrix.length; i++) {
                 for (let j = 0; j < matrix[i].length; j++) {
+
                     let current_box = matrix[i][j];
                     if (current_box.done == true) {
                         ctx.drawImage(rotated_images[current_box.possibilities[0]], j * 50, i * 50, cellSize, cellSize);
                     }
+
                 }
             }
 
@@ -617,19 +641,29 @@ function solve_next_cell(matrix) {
     // Get boxes with minimum entropy (possibilities) -- ignore solved cells
     let lowestEntropy = 0;
     let lowEntropyCells = [];
+
     for (let i = 0; i < matrix.length; i++) {
         for (let j = 0; j < matrix[i].length; j++) {
+
             let currentBox = matrix[i][j];
             let currentEntropy = currentBox.possibilities.length;
+
             if (!currentBox.done) {
+
                 if (lowEntropyCells.length == 0 || currentEntropy == lowestEntropy) {
+
                     lowestEntropy = currentEntropy;
                     lowEntropyCells.push(currentBox);
+
                 } else if (currentEntropy < lowestEntropy) {
+
                     lowestEntropy = currentEntropy;
                     lowEntropyCells = [currentBox];
+
                 }
+
             }
+
         }
     }
 
@@ -641,6 +675,8 @@ function solve_next_cell(matrix) {
 }
 
 function shuffle(array) {
+
+    // Simple shuffling function to give randomness to which cell we're solving.
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
         [array[i], array[j]] = [array[j], array[i]];
@@ -651,17 +687,23 @@ function recalculate_constraints(matrix, box) {
 
     const x = box.x;
     const y = box.y;
+
+    // Grab neighbor possibilities if a neighbor exists and hasn't been determined.
     const top_possibilities = (y-1 >= 0 && !matrix[y-1][x].done) ? matrix[y-1][x].possibilities : null;
     const right_possibilities = (x+1 < collapse_width && !matrix[y][x+1].done) ? matrix[y][x+1].possibilities : null;
     const bottom_possibilities = (y+1 < collapse_height && !matrix[y+1][x].done) ? matrix[y+1][x].possibilities : null;
     const left_possibilities = (x-1 >= 0 && !matrix[y][x-1].done) ? matrix[y][x-1].possibilities : null;
+
+    // Shuffle the chosen box's possibilities for randomness.
     shuffle(box.possibilities);
 
+    // Test each possibile image for box to see how much entropy it removes.
     for (let i = 0; i < box.possibilities.length; i++) {
 
         let constraints = cells[box.possibilities[i]];
         let side_scores = [];
 
+        // Get the top, right, bottom, and left possibilities after the box is set to the chosen image.
         if (top_possibilities != null) {
             let top_score = top_possibilities.filter((element) => constraints.top.includes(element)).length;
             side_scores.push(top_score);
@@ -682,10 +724,13 @@ function recalculate_constraints(matrix, box) {
             side_scores.push(left_score);
         }
 
+        // If all the neighbors are done or non-existent, accept the image.
         if (side_scores == []) {
             box.done = true;
             box.possibilities = [box.possibilities[i]];
             return;
+
+        // Otherwise, check to see if any neighbor boxes would have zero possibilities (heuristic). If not, accept the image.
         } else {
 
             if (side_scores.indexOf(0) == -1) {
@@ -730,6 +775,7 @@ function test_rotated_images() {
     let x = 0;
     let y = 0;
 
+    // Print all the rotated images.
     for (let i = 0; i < rotated_images.length; i++) {
         ctx.drawImage(rotated_images[i], x, y, 50, 50);
         x += 50;
@@ -741,6 +787,7 @@ function test_rotated_images() {
 
     y += 100;
 
+    // Print all the cells.
     for (let i = 0; i < cells.length; i++) {
         ctx.drawImage(cells[i].image, x, y, 50, 50);
         x += 50;
@@ -755,6 +802,8 @@ function test_rotated_images() {
 function generate() {
 
     if (!generate_ready) return;
+
+    // For debugging:
     // test_rotated_images();
     // return;
 
@@ -771,41 +820,36 @@ function generate() {
     }
     draw_current_state(matrix, "fast");
 
-    // Fill the matrix box-by-box
-
     let c = 0;
     let max = collapse_height * collapse_width;
 
+    // Async waiting for promise from draw_current_state -- continues once stage is drawn if that setting is selected.
     async function next_matrix() {
 
         while (c < max) {
-            c++;
     
             let solved_box = solve_next_cell(matrix);
+            c++;
+
             // Restart generation if any box has zero possible images that fit constraints
             if (recalculate_constraints(matrix, solved_box) == -1) {
-                // console.log("Dead-end. Regenerating.");
-                // draw_current_state(matrix);
-                // break;
                 generate();
                 break;
             }
-    
             await draw_current_state(matrix, generation_mode);
         }
-
     }
-    
+
+    // Fill the matrix box-by-box
     next_matrix();
-
-    // let max = collapse_height * collapse_width;
-    // next_matrix(matrix, max, 0);
-
 }
 
 //#endregion
 
+// Resize for to make sure all fields are properly set.
 window.dispatchEvent(new Event('resize'));
+
+// Load the default image set.
 load_new_images();
 
 
